a19df92d959e5ae7b4eb7b745f3157ba5a5deab1
Le Minh <le.minh011100@gmail.com>
2022-05-28 14:47:26 +0700

    fixed access bug

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export default axios.create({
   baseURL: 'https://ezexpress.tk/https://ga-mobile-api.loklok.tv/cms/app',
   headers: {
